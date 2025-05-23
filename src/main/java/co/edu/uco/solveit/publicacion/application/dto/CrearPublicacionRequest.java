7efa549964555c9e311f3dc08ba19aa84ec80ca0
Juan Manuel <juan.zuluaga4461@uco.net.co>
2025-05-22 19:52:01 -0500

    Remove unused test class and redundant field from DTO
    Deleted the unused `SolveitApplicationTests` class to clean up the test directory. Also, removed the `usuarioId` field from `CrearPublicacionRequest` as it is no longer required.

--- src/main/java/co/edu/uco/solveit/publicacion/application/dto/CrearPublicacionRequest.java
+++ src/main/java/co/edu/uco/solveit/publicacion/application/dto/CrearPublicacionRequest.java
@@ -4,7 +4,6 @@ package co.edu.uco.solveit.publicacion.application.dto;
 import co.edu.uco.solveit.publicacion.domain.model.TipoPublicacion;
 
 public record CrearPublicacionRequest(
-    Long usuarioId,
     String titulo,
     String descripcion,
     TipoPublicacion tipoPublicacion,
